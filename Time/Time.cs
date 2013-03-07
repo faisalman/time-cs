b74c50e321481e233cae29ee89f0c2552e599ccd
Faisal Salman <fyzlman@gmail.com>
2013-03-08 02:25:27 +0700

    Add DateTime.GetAge() extension method

--- Time/Time.cs
+++ Time/Time.cs
@@ -9,6 +9,25 @@ using System;
 
 namespace Faisalman.Utils
 {
+  public struct Age
+  {
+    public int Years;
+    public int Months;
+    public int Days;
+    public Age(int years, int months, int days)
+    {
+      this.Years = years;
+      this.Months = months;
+      this.Days = days;
+    }
+    public override string ToString()
+    {
+      return this.Years.ToString() + " " + (this.Years > 1 ? "years" : "year")
+        + " " + this.Months.ToString() + " " + (this.Months > 1 ? "months" : "month")
+        + " " + this.Days.ToString() + " " + (this.Days > 1 ? "days": "day");
+    }
+  }
+
   public static class Time
   {
     const int ONE_SECOND = 1;
@@ -18,6 +37,52 @@ namespace Faisalman.Utils
     const int ONE_WEEK = 7 * ONE_DAY;
     const int ONE_MONTH = 30 * ONE_DAY;
     const int ONE_YEAR = 12 * ONE_MONTH;
+
+    public static Age GetAge(this DateTime bday, DateTime cday)
+    {
+      Age age = new Age();
+      
+      if ((cday.Year - bday.Year) > 0 ||
+          (((cday.Year - bday.Year) == 0) && ((bday.Month < cday.Month) ||
+            ((bday.Month == cday.Month) && (bday.Day <= cday.Day)))))
+      {
+        int daysInBdayMonth = DateTime.DaysInMonth(bday.Year, bday.Month);
+        int daysRemain = cday.Day + (daysInBdayMonth - bday.Day);
+
+        if (cday.Month > bday.Month)
+        {
+          age.Years = cday.Year - bday.Year;
+          age.Months = cday.Month - (bday.Month + 1) + Math.Abs(daysRemain / daysInBdayMonth);
+          age.Days = (daysRemain % daysInBdayMonth + daysInBdayMonth) % daysInBdayMonth;
+        }
+        else if (cday.Month == bday.Month)
+        {
+          if (cday.Day >= bday.Day)
+          {
+            age.Years = cday.Year - bday.Year;
+            age.Months = 0;
+            age.Days = cday.Day - bday.Day;
+          }
+          else
+          {
+            age.Years = (cday.Year - 1) - bday.Year;
+            age.Months = 11;
+            age.Days = DateTime.DaysInMonth(bday.Year, bday.Month) - (bday.Day - cday.Day);
+          }
+        }
+        else
+        {
+          age.Years = (cday.Year - 1) - bday.Year;
+          age.Months = cday.Month + (11 - bday.Month) + Math.Abs(daysRemain / daysInBdayMonth);
+          age.Days = (daysRemain % daysInBdayMonth + daysInBdayMonth) % daysInBdayMonth;
+        }
+      }
+      else
+      {
+        throw new ArgumentException("Birthday date must be earlier than current date");
+      }
+      return age;
+    }
 				
     public static string GetRelative(this DateTime currentTime, DateTime measuredTime)
     {
